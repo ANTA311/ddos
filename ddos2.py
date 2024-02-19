0477ca47c4b38ee41f9593606dd4ba7b172fb67b
Antahezi <147943100+ANTA311@users.noreply.github.com>
2024-02-19 08:46:59 +0800

    Update and rename ddos3.py to ddos2.py

---
+++ ddos2.py
@@ -0,0 +1,50 @@
+#python3
+按键移动焦点。或者，使用 sys
+欧洲心脏病学会 os
+然后 time
+突耳 socket
+移动到页面上的下一个交互元素。编辑ddos3.天
+进口系统
+导入os导入os进口时间 datetime
+月()
+进口插座进口
+进口进口
+现在
+从datetime导入now = datetime.现在()
+now = datetime.托座]DDos攻击]）()小时=现在
+
+##############
+小时='[版本 1.0]'(socket.AF_INET, socket.SOCK_DGRAM)
+）（打印（'[Github:https://Github.com/ANTA311/ddos]'）（打印（'[Github:https://Github.com/ANTA311/ddos]'）(1490)
+#############
+
+os.打印（'[[you mayou mayoto mayou mayou）]'）（IP=input([[[you mayou IP：]]）系统（[figlet Antahezi DDos moto]）port=input([[[briglet Antahezi DDos you]：]）（port=int(port)））（IP=打印（[Antahezi]]）you（[figlet Antahezi DDos you]）（[[]0％]）([[[you mayou IP：]]） = now.打印（'[mayoto mayoto 1.0]'）（[======]25％]）（端口）[figlet Antahezi DDos moto]）port=os.使用（“清除”）([[[briglet Antahezi DDos you]：]）（port=打印（'[Github:https：//Github. com/ANTA311/ddos]'）'）++you+m(//Github. com)(port)）("clear")
+day = now.突耳(按键移动焦点。或者，使用)[Antahezi]]）you（[figlet Antahezi DDos you]）（[[]0％]）=打印（托座DDos攻击]）
+）（[======]25％]）（端口）（端口=you）
+）（打印（'[Github:https://Github.com/ANTA311/ddos]'袜子=插座。os.时间.欧洲心脏病学会（1）[[====================]100％]）[you yousent = 0[Github:https：//Github. 然后(突耳)/ANTA311/ddos]sendto（字节，（ip，bioto））
+系统（[figlet Antahezi briomas briolomas.虽然属实： = sent + 1[Antahezi]]）
+打印[***]
+现在
+#代码时间
+现在
+
+月
+现在
+托座
+）
+'[版本 1.0]'
+）
+打印（
+'[[you mayou mayoto mayou mayou）]'
+输入
+输入
+伊
+打印（
+打印（[[=====================]100％]）
+sent = 0
+月
+输入
+伊
+#代码时间
+端口
+如果端口
